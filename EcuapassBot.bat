610e0f7b66a05eb22f1520f4569f03eea585a108
lgarreta <lgarreta@gmail.com>
2025-01-24 07:51:40 -0500

    Nov/24: r0.937: Removed

--- EcuapassBot.bat
+++
@@ -1,8 +0,0 @@
-taskkill /IM "ecuapass_server.exe" /F
-
-echo "Actualizando EcuapassBot..."
-
-git reset --hard 
-git pull origin main
-
-EcuapassBotGUI.exe
--- FirstUpdate.bat
+++
@@ -1,5 +0,0 @@
-:: Initialize git repo and fetch latest release
-git init
-git remote add origin https://github.com/lgarreta/EcuapassBot5-win.git
-git fetch origin main
-git reset --hard origin/main
